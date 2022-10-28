c307764106ba826240f34e1643a676928c9c7915
DavidYauri1 <david.yauri@tecsup.edu.pe>
2022-10-27 23:56:43 -0500

    Crud cursos part2

--- resources/views/layouts/instructor.blade.php
+++ resources/views/layouts/instructor.blade.php
@@ -29,11 +29,11 @@
                 <aside>
                     <h1>Edicion del curso</h1>
                     <ul class="text-sm text-gray-600">
-                        <li class="leading-7 mb-1 border-l-4 border-indigo-400 pl-2">
-                            <a href="" class="font-bold text-lg mb-4">informacion del curso</a>
+                        <li class="leading-7 mb-1 border-l-4 border-indigo-400  @routeIs('instructor.courses.edit',$course) border-indigo-400 @else border-transparent @endif pl-2 ">
+                            <a href="{{ route('instructor.courses.edit',$course) }}" class="font-bold text-lg mb-4">informacion del curso</a>
                         </li>
-                        <li class="leading-7 mb-1 border-l-4 border-transparent pl-2">
-                            <a href="">Lecciones</a>
+                        <li class="leading-7 mb-1 border-l-4 @routeIs('instructor.courses.curriculum',$course) border-indigo-400 @else border-transparent @endif pl-2">
+                            <a href="{{ route('instructor.courses.curriculum',$course) }}">Lecciones</a>
                         </li>
                         <li class="leading-7 mb-1 border-l-4  border-transparent pl-2">
                             <a href="">Metas del curso</a>
@@ -46,78 +46,11 @@
                 
                 <main class="col-span-4 card"> 
                     <div class="card-body text-gray-600 ">
-                        <h1 class="text-2xl font-bold">Informacion del curso</h1>
-                        <hr class="mt-2 nb-6">
-                        {!! Form::model($course, ['route' => ['instructor.courses.update',$course], 'method' => 'put','files' => true]) !!}
-                        @include('instructor.courses.partials.form')
-                     
-                        <div class="flex justify-end">
-                            {!! Form::submit('Actualizar informacion', ['class' => 'btn btn-primary']) !!}
-                        </div>
-        
-        
-                        {!! Form::close() !!}
+                     {{ $slot }}
                     </div>
                 </main>
         
             </div>
-        
-                <x-slot name="js">
-                    <script src="https://cdn.ckeditor.com/ckeditor5/35.1.0/classic/ckeditor.js"></script>
-                    <script src="js/instructor/courses/"></script>            
-                    <script>
-                                  document.getElementById("title").addEventListener('keyup', slugChange);
-        
-        function slugChange(){
-            
-            title = document.getElementById("title").value;
-            document.getElementById("slug").value = slug(title);
-        
-        }
-        
-        function slug (str) {
-            var $slug = '';
-            var trimmed = str.trim(str);
-            $slug = trimmed.replace(/[^a-z0-9-]/gi, '-').
-            replace(/-+/g, '-').
-            replace(/^-|-$/g, '');
-            return $slug.toLowerCase();
-        }
-        
-        
-        
-                     ClassicEditor
-                        .create( document.querySelector( '#description' ), {
-                            toolbar: [ 'heading', '|', 'bold', 'italic', 'link', 'blockQuote' ],
-                            heading: {
-                                options: [
-                                    { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
-                                    { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
-                                    { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' }
-                                ]
-                            }
-                        } )
-                        .catch( error => {
-                            console.log( error );
-                        } );
-        
-                        //Cambiar imagen
-                    document.getElementById("file").addEventListener('change', cambiarImagen);
-        
-                    function cambiarImagen(event){
-                        var file = event.target.files[0];
-        
-                        var reader = new FileReader();
-                        reader.onload = (event) => {
-                            document.getElementById("picture").setAttribute('src', event.target.result); 
-                        };
-        
-                        reader.readAsDataURL(file);
-                    }
-        
-                  </script>
-                            
-                 </x-slot>
         </div>
 
         @stack('modals')
--- routes/instructor.php
+++ routes/instructor.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\HomeController;
 use App\Http\Controllers\Instructor\CourseController;
+use App\Http\Livewire\Instructor\CoursesCurriculum;
 use App\Http\Livewire\InstructorCourses;
 use Illuminate\Support\Facades\Route;
 
@@ -11,3 +12,4 @@ Route::redirect('','instructor/courses');
 
 
 Route::resource('courses', CourseController::class)->names('courses');
+Route::get('courses/{course}/curriculum', CoursesCurriculum::class )->name('courses.curriculum');
